8d0529e3ddc915472b2855cb406788aa3e2d0700
Eduardo <eduardo.benigno15@gmail.com>
2026-09-14 15:25:43 -0300

    feat: class 0609

--- 0609_authentication-middleware/src/utils/jwt.ts
+++ 0609_authentication-middleware/src/utils/jwt.ts
@@ -1,6 +1,7 @@
-import { SignJWT } from 'jose'
+import { jwtVerify, SignJWT } from 'jose'
 import { createSecretKey } from 'crypto'
-import env from '../../env.ts'
+import env from '../../../env.ts'
+import type { isReturnStatement } from 'typescript'
 
 export interface JwtPayload {
   id: string
@@ -17,4 +18,11 @@ export const generateToken = (payload: JwtPayload) => {
     .setIssuedAt()
     .setExpirationTime(env.JWT_EXPIRES_IN || '7d')
     .sign(secretKey)
+}
+
+export const verifyToken = async (token: string): Promise<JwtPayload> => {
+  const secretKey = createSecretKey(env.JWT_SECRET, 'utf-8')
+  const { payload } = await jwtVerify(token, secretKey)
+
+  return payload as unknown as JwtPayload
 }
