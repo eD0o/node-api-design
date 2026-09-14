d3783650775a1f118c3ee4c7601b03d240b6937b
Eduardo <eduardo.benigno15@gmail.com>
2026-09-14 17:40:19 -0300

    feat: class 0701

--- 0604_create-a-jwt/src/utils/password.ts
+++
@@ -1,6 +0,0 @@
-import bcrypt from 'bcrypt'
-import env from '../../../env.ts'
-
-export const hashPassword = async (password: string): Promise<string> => {
-  return bcrypt.hash(password, env.BCRYPT_ROUNDS)
-}
